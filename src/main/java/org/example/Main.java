e3c97acd4a1ea80dcf059bf1b276b16592a455f2
Mousaei <vahidmfr@yahoo.fr>
2024-01-19 13:40:10 +0100

    initialize natixis 1.0

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,19 +8,68 @@ public class Main {
 
         ShopCart shopCart = new ShopCart();
         List<Product> products = new ArrayList<>();
+        List<Integer> quantities = new ArrayList<>();
+        List<Float> total = new ArrayList<>();
 
-        for(int i = 0; i<5; i++) {
+        float totalProduct =0F;
+
+        StringBuilder jsonBuffer = new StringBuilder();
+
+        for(int i = 0; i<3; i++) {
             Product product = new Product();
             product.setName("Pen");
             product.setPrice(1.50F);
+            products.add(product);
+
+
+            totalProduct = totalProduct + 1.50F;
+
+
         }
 
-        for(int i = 0; i<5; i++) {
+        quantities.set(0 , new Integer(3));
+        total.set(0, totalProduct);
+
+        shopCart.setProducts(products);
+        shopCart.setQuantity(quantities);
+        shopCart.setTotal(total);
+
+
+        for(int i = 0; i<2; i++) {
             Product product = new Product();
             product.setName("Book");
             product.setPrice(8F);
+            products.add(product);
+
+            quantities.add(new Integer(i));
+            totalProduct = totalProduct + 8F;
+
+            total.add(totalProduct);
         }
 
-        System.out.println("Hello world!");
+        quantities.set(1 , new Integer(2));
+        total.set(1 , totalProduct);
+
+        shopCart.setProducts(products);
+        shopCart.setQuantity(quantities);
+        shopCart.setTotal(total);
+
+
+        System.out.println("Product    Quantity    Price    Total");
+        jsonBuffer.append("Product    Quantity    Price    Total");
+        System.out.println("-------------------------------------------");
+        jsonBuffer.append("-------------------------------------------");
+
+        String beforeName =  "";
+
+        Product beforeProduct = new Product();
+
+        for(int i = 0; i < 5; i++) {
+            System.out.println( (shopCart.getProducts().get(0)).getName() + "    " + (shopCart.getQuantity().get(i)).toString() +
+                    "    " + (shopCart.getProducts().get(i)).getPrice() + "    " +  (shopCart.getTotal().get(i)).toString() );
+
+            jsonBuffer.append((shopCart.getProducts().get(0)).getName() + "    " + (shopCart.getQuantity().get(i)).toString() +
+                    "    " + (shopCart.getProducts().get(i)).getPrice() + "    " +  (shopCart.getTotal().get(i)).toString() );
+        }
     }
 }
--- src/main/java/org/example/ShopCart.java
+++ src/main/java/org/example/ShopCart.java
@@ -5,44 +5,31 @@ import java.util.Objects;
 
 public class ShopCart {
 
-    private int quantity;
-    private List Product;
-    private float total;
+    private List<Integer> quantity;
+    private List<Product> Products;
+    private List<Float> total;
 
-    public int getQuantity() {
+    public List<Integer> getQuantity() {
         return quantity;
     }
 
-    public void setQuantity(int quantity) {
+    public void setQuantity(List<Integer> quantity) {
         this.quantity = quantity;
     }
 
-    public List getProduct() {
-        return Product;
+    public List<Product> getProducts() {
+        return Products;
     }
 
-    public void setProduct(List product) {
-        Product = product;
+    public void setProducts(List<Product> products) {
+        Products = products;
     }
 
-    public float getTotal() {
+    public List<Float> getTotal() {
         return total;
     }
 
-    public void setTotal(float total) {
+    public void setTotal(List<Float> total) {
         this.total = total;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof ShopCart)) return false;
-        ShopCart shopCart = (ShopCart) o;
-        return getQuantity() == shopCart.getQuantity() && Float.compare(getTotal(), shopCart.getTotal()) == 0 && Objects.equals(getProduct(), shopCart.getProduct());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getQuantity(), getProduct(), getTotal());
     }
 }
