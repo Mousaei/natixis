3182ea9fa86d8c1e2567114da882f8716e614b73
Mousaei <vahidmfr@yahoo.fr>
2024-01-19 17:24:37 +0100

    initialize natixis 1.0

--- src/test/java/ShopCartTest.java
+++ src/test/java/ShopCartTest.java
@@ -2,7 +2,6 @@ import com.natixis.Product;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
 
 public class ShopCartTest {
 
@@ -18,10 +17,8 @@ public class ShopCartTest {
     @Test
     public void tesPriceProduct() {
         Product aPen = new Product();
-        aPen.setPrice(1.50F);
+        aPen.setName("Book");
 
-        System.out.println("secondPen: " + aPen.getPrice());
-        // Assert
-        assertSame( aPen, aPen);
+        assertEquals(aPen.getName(), "Book");
     }
 }
